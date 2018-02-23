8507fb3b8433cf90abd25bbcbab6e0ce2bf66cb3
xquire24 <xquire24@gmail.com>
2018-02-22 23:47:25 -0800

    edit App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Todo List Application</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <MyComponent/>
         </p>
       </div>
     );
