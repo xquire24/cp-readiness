771996f75ce06b4f7e3fc2ef870beac505ab1a98
xquire24 <xquire24@gmail.com>
2018-02-22 23:09:27 -0800

    add component file

---
+++ todo-app/src/MyComponent.js
@@ -0,0 +1,14 @@
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        term: '',
+        items: []
+      };
+    }
+    render() {
+        return (
+          <div></div>
+        );
+      }
+    }
